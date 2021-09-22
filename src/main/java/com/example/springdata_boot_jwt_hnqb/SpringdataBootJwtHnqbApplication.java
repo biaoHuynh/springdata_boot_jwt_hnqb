6bfa751b1e0cf1659f60594f92b2796bcb3ad21d
biaoHuynh <89762253+biaoHuynh@users.noreply.github.com>
2021-09-22 08:23:17 +0700

    commit 1

---
+++ src/main/java/com/example/springdata_boot_jwt_hnqb/SpringdataBootJwtHnqbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdata_boot_jwt_hnqb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataBootJwtHnqbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataBootJwtHnqbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdata_boot_jwt_hnqb/SpringdataBootJwtHnqbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdata_boot_jwt_hnqb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataBootJwtHnqbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
